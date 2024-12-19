1d9f2383741c2ea44672d5345d8447942b2da8d6
DrFIour <157080251+DrFIour@users.noreply.github.com>
2024-12-19 15:07:22 -0600

    Delete BackEnd/secrets.js

--- BackEnd/secrets.js
+++
@@ -1,9 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://shaoboh2:hsb000509@cluster0.meto4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
-    gpt_api_key : ""
-};
